3cdc6ce6ee20b765b7760576c9ab270e3aaba9d1
akbarsaputrait <akbarsaputrait@outlook.com>
2024-12-31 19:54:19 +0700

    initial commit

---
+++ projects/lib/src/ngememoize/ngememoize.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NgememoizeService {
+  constructor() {}
+
+  hello() {
+    return 'hello';
+  }
+}
---
+++ projects/lib/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of lib
+ */
+
+export * from './ngememoize/ngememoize.service';
+export * from './ngememoize/ngememoize.component';
