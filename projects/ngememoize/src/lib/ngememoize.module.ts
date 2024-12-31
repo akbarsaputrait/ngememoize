b2bcd2ebfd6110b2fb9e596474afcaafb36a2877
akbarsaputrait <akbarsaputrait@outlook.com>
2025-01-01 02:29:57 +0700

    refactor: remove unused

--- projects/ngememoize/src/lib/ngememoize.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { NgememoizeService } from './ngememoize.service';
-
-@NgModule({
-  declarations: [],
-  providers: [NgememoizeService],
-  exports: [],
-})
-export class NgememoizeModule {}
