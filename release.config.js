22371f60a195638151590c4547dcc9f12f81d248
akbarsaputrait <akbarsaputrait@outlook.com>
2025-01-02 22:07:52 +0700

    fix: bug peer-dep

---
+++ release.config.js
@@ -0,0 +1,23 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+  branches: [
+    "master",
+    { name: "beta", prerelease: true },
+    { name: "testing", prerelease: true },
+  ],
+  repositoryUrl: "https://github.com/akbarsaputrait/ngememoize",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: true,
+        tarballDir: "dist/ngememoize",
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
